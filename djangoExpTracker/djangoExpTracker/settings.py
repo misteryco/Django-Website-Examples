a60d39df4e4a04ad8a62e2e1aad81e4d9c47e792
Yordan Dimov <102857417+misteryco@users.noreply.github.com>
2022-11-11 13:15:53 +0200

    Update settings.py

--- djangoExpTracker/djangoExpTracker/settings.py
+++ djangoExpTracker/djangoExpTracker/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ol^ujb!_s+2%iba&=w27b-e^p9f9pnr)p@ug6=+^*ro%=1x*gi'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
