0ad33d472f9d70b396807fe34df832076a462f85
Yordan Dimov <102857417+misteryco@users.noreply.github.com>
2022-11-11 13:13:31 +0200

    Update settings.py

--- CarCollectionApp/CarCollectionApp/settings.py
+++ CarCollectionApp/CarCollectionApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # put secret key below
-SECRET_KEY = '12345'
+SECRET_KEY =  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
